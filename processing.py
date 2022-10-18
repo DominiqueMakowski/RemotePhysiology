e772f8f6a2c79c8ff1aac58d4856e1b690baa27b
Dominique Makowski <dom.mak19@gmail.com>
2022-10-18 17:29:04 +0800

    Update processing.py

--- processing.py
+++ processing.py
@@ -61,72 +61,74 @@ def extract_face(path, classifier="haarcascade_frontalface_alt0.xml", blur=3):
     return np.array(frames), sampling_rate
 
 
-def webcam_heartrate(frames, sampling_rate=30):
-    raw = np.mean(np.mean(np.mean(frames, axis=1), axis=1), axis=1)
+# def webcam_heartrate(frames, sampling_rate=30):
+#     raw = np.mean(np.mean(np.mean(frames, axis=1), axis=1), axis=1)
 
-    # Clean
-    raw = nk.signal_filter(
-        raw,
-        sampling_rate=sampling_rate,
-        lowcut=1,
-        highcut=1.8,
-    )
+#     # Clean
+#     raw = nk.signal_filter(
+#         raw,
+#         sampling_rate=sampling_rate,
+#         lowcut=1,
+#         highcut=1.8,
+#     )
 
-    iff = scipy.fftpack.ifft(scipy.fftpack.fft(frames, axis=0), axis=0)
-    iff = np.mean(np.mean(np.mean(iff, axis=1), axis=1), axis=1)
-    return raw, iff
-
-
-def temporal_ideal_filter(tensor, sampling_rate=30, low=0.4, high=2, axis=0):
-    fft = scipy.fftpack.fft(tensor, axis=axis)
-    frequencies = scipy.fftpack.fftfreq(tensor.shape[0], d=1.0 / sampling_rate)
-    bound_low = (np.abs(frequencies - low)).argmin()
-    bound_high = (np.abs(frequencies - high)).argmin()
-    fft[:bound_low] = 0
-    fft[bound_high:-bound_high] = 0
-    fft[-bound_low:] = 0
-    iff = scipy.fftpack.ifft(fft, axis=axis)
-    return np.abs(iff)
+#     iff = scipy.fftpack.ifft(scipy.fftpack.fft(frames, axis=0), axis=0)
+#     iff = np.mean(np.mean(np.mean(iff, axis=1), axis=1), axis=1)
+#     return raw, iff
 
 
-def webcam_heartrate(frames, sampling_rate=30):
-    """full signal processing pipeline"""
-    # Average all pixels in the different colors
-    mixed_signals = np.mean(frames, axis=(1, 2))  # (len, color)
-    raw = np.mean(mixed_signals, axis=1)
+# def temporal_ideal_filter(tensor, sampling_rate=30, low=0.4, high=2, axis=0):
+#     fft = scipy.fftpack.fft(tensor, axis=axis)
+#     frequencies = scipy.fftpack.fftfreq(tensor.shape[0], d=1.0 / sampling_rate)
+#     bound_low = (np.abs(frequencies - low)).argmin()
+#     bound_high = (np.abs(frequencies - high)).argmin()
+#     fft[:bound_low] = 0
+#     fft[bound_high:-bound_high] = 0
+#     fft[-bound_low:] = 0
+#     iff = scipy.fftpack.ifft(fft, axis=axis)
+#     return np.abs(iff)
 
-    # Filter signals
-    def preprocess(signal, sampling_rate=sampling_rate):
-        return nk.signal_filter(
-            signal,
-            sampling_rate=sampling_rate,
-            lowcut=1,
-            highcut=2,
-        )
 
-    normalized = np.apply_along_axis(preprocess, 0, mixed_signals)  # detrend and normalize
+# def webcam_heartrate(frames, sampling_rate=30):
+#     """full signal processing pipeline"""
+#     # Average all pixels in the different colors
+#     mixed_signals = np.mean(frames, axis=(1, 2))  # (len, color)
+#     raw = np.mean(mixed_signals, axis=1)
 
-    nk.signal_plot(
-        [normalized[:, 0], normalized[:, 1], normalized[:, 2]], sampling_rate=sampling_rate
-    )
+#     # Filter signals
+#     def preprocess(signal, sampling_rate=sampling_rate):
+#         return nk.signal_filter(
+#             signal,
+#             sampling_rate=sampling_rate,
+#             lowcut=1,
+#             highcut=2,
+#         )
 
-    ica = sklearn.decomposition.FastICA(n_components=1, max_iter=1000)
-    ica_transformed = ica.fit_transform(normalized)
-    nk.signal_plot(ica_transformed[:, 0], sampling_rate=sampling_rate)
-    nk.signal_plot(
-        [ica_transformed[:, 0], ica_transformed[:, 1], ica_transformed[:, 2]],
-        sampling_rate=sampling_rate,
-    )
+#     normalized = np.apply_along_axis(preprocess, 0, mixed_signals)  # detrend and normalize
 
-    return ica_transformed
+#     nk.signal_plot(
+#         [normalized[:, 0], normalized[:, 1], normalized[:, 2]], sampling_rate=sampling_rate
+#     )
+
+#     ica = sklearn.decomposition.FastICA(n_components=1, max_iter=1000)
+#     ica_transformed = ica.fit_transform(normalized)
+#     nk.signal_plot(ica_transformed[:, 0], sampling_rate=sampling_rate)
+#     nk.signal_plot(
+#         [ica_transformed[:, 0], ica_transformed[:, 1], ica_transformed[:, 2]],
+#         sampling_rate=sampling_rate,
+#     )
+
+#     return ica_transformed
 
 
 frames, sampling_rate = extract_face("video.mp4")
 plt.imshow(frames[100, :, :, 0])
-filtered_tensor = temporal_ideal_filter(frames, sampling_rate=sampling_rate)
-plt.imshow(filtered_tensor[100, :, :, 0])
-
-raw, iff = webcam_heartrate(frames, sampling_rate)
 
 
-nk.signal_plot([raw, iff.real], sampling_rate=sampling_rate, title="Heart Rate", standardize=True)
+# filtered_tensor = temporal_ideal_filter(frames, sampling_rate=sampling_rate)
+# plt.imshow(filtered_tensor[100, :, :, 0])
+
+# raw, iff = webcam_heartrate(frames, sampling_rate)
+
+
+# nk.signal_plot([raw, iff.real], sampling_rate=sampling_rate, title="Heart Rate", standardize=True)
