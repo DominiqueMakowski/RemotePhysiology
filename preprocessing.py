8b4c886a745145203bb75be181864b50467e233a
Dominique Makowski <dom.mak19@gmail.com>
2022-10-25 14:31:30 +0800

    example video

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@ import glob
 
 import neurokit2 as nk
 import pandas as pd
+import pyVHR.analysis.pipeline
 
 # 1. Convert json video data to mp4
 # ==================================
@@ -16,16 +17,47 @@ for file in glob.glob("data/*.json"):
         f.write(video)
         f.close()
 
-# 2. Preprocess physio
-# ==================================
+# 2. Preprocess physio (stored as .csv)
+# =====================================
 for file in glob.glob("data/*.txt"):
     print(file)
     data, info = nk.read_bitalino(file)
     sampling_rate = info["sampling_rate"]
 
-    lux = data["LUX"]
-    nk.signal_plot(lux, sampling_rate=sampling_rate)
+    # Get onset and end
+    lux = data["LUX"].values
     events = nk.events_find(lux, threshold="auto", threshold_keep="below", duration_min=10000)
 
-    print(f"duration: {events['duration'][0] / sampling_rate / 60}")
-    data = data.iloc[events["onset"][0] : events["onset"][0] + events["duration"][0]]
+    print(f"- duration: {events['duration'][0] / sampling_rate / 60}")
+    if events["duration"][0] < 300000:
+        print(f"Skipping for {file}")
+        continue
+
+    # Process
+    ecg, _ = nk.ecg_process(data["ECGB"], sampling_rate=sampling_rate)
+    ppg, _ = nk.ppg_process(data["PULS"], sampling_rate=sampling_rate)
+    rsp, _ = nk.rsp_process(data["RESP"], sampling_rate=sampling_rate)
+
+    dat = pd.concat(
+        [
+            ecg[["ECG_Clean", "ECG_Rate"]],
+            ppg[["PPG_Clean", "PPG_Rate"]],
+            rsp[["RSP_Clean", "RSP_Rate"]],
+        ],
+        axis=1,
+    )
+
+    # Truncate
+    dat = dat.iloc[events["onset"][0] : events["onset"][0] + events["duration"][0]]
+
+    # 3. Benchmark using pyVHR
+    # ======================
+    pipe = pyVHR.analysis.pipeline.Pipeline()
+    time, BPM, uncertainty = pipe.run_on_video(
+        file.replace(".txt", ".mp4"), roi_approach="patches", roi_method="faceparsing"
+    )
+
+    dat["pyVHR"] = nk.signal_resample(BPM, desired_length=len(dat))
+
+    # Save
+    dat.to_csv(file.replace(".txt", ".csv"), index=False)
--- video_rppg.py
+++ video_rppg.py
@@ -1,55 +1,56 @@
+import glob
+
 import matplotlib.pyplot as plt
 import numpy as np
-from pyVHR.analysis.pipeline import Pipeline
+import pandas as pd
 
-# Benchmark
+for file in glob.glob("data/*.csv"):
+    print(file)
+
+
 # ===================
-pipe = Pipeline()
-time, BPM, uncertainty = pipe.run_on_video(
-    "data/video.mp4", roi_approach="patches", roi_method="faceparsing"
-)
+# Ground truth
+data = pd.read_csv(file)
 
-plt.figure()
-plt.plot(time, BPM)
-plt.fill_between(time, BPM - uncertainty, BPM + uncertainty, alpha=0.2)
-plt.show()
-# ===================
-
-video, sampling_rate = nk.read_video("data/video.mp4")
-
-signal = nk.ecg_simulate(duration=int(len(video) / sampling_rate), sampling_rate=200)
-signal = nk.signal_resample(signal, desired_length=len(video), sampling_rate=200)
-nk.video_plot(video, frames=3, signals=signal)
-nk.video_plot(video, frames=3, signals=[signal, signal])
-nk.video_plot([video, video], frames=3, signals=[signal, signal])
+# for i in range(3):
+#     start = i * 60000
+#     nk.signal_plot(
+#         [
+#             data["pyVHR"][start : start + 60000],
+#             data["ECG_Rate"][start : start + 60000],
+#             data["PPG_Rate"][start : start + 60000],
+#         ],
+#         sampling_rate=1000,
+#     )
 
 
-# Plane-Orthogonal-to-Skin (POS)
-# ==============================
-# 4. Spatial averaging
-C = np.mean(frames, axis=(1, 2))  # (len, color)
-S = np.zeros((len(frames), 2))
+video, sampling_rate = nk.read_video(file.replace(".csv", ".mp4"))
 
-for n, Ci in enumerate(C):
-    if n > sampling_rate:
-        # 5. Temporal normalization
-        Cn = Ci / np.mean(C[n - sampling_rate : n + 1], axis=0)
-    else:
-        Cn = Ci / np.mean(C[0 : n + 1], axis=0)
+# Crop
+vid = video[0 : sampling_rate * 3, :, :, :]
+dat = data[0 : 1000 * 3]
 
-    # 6. Projection
-    projection_matrix = np.array([[0, 1, -1], [-2, 1, 1]])
-    S[n, :] = np.matmul(projection_matrix, Cn)
+# Save example
+np.savez("example", vid)
+dat.to_csv("example.csv", index=False)
 
-h = S[:, 0] + (np.std(S[:, 0]) / np.std(S[:, 1])) * S[:, 1]
+# Faces
+faces = nk.video_face(vid)
+ppg = nk.video_ppg(vid)
 
-nk.signal_plot(
-    [
-        h,
-        nk.signal_filter(h, sampling_rate=sampling_rate, lowcut=1, highcut=1.8),
-        np.mean(C, axis=1),
-        nk.signal_filter(np.mean(C, axis=1), sampling_rate=sampling_rate, lowcut=1, highcut=1.8),
+
+nk.video_plot(
+    [vid, faces],
+    frames=5,
+    signals=[
+        data["PPG_Clean"],
+        nk.signal_filter(
+            nk.signal_resample(ppg, desired_length=len(dat)),
+            sampling_rate=1000,
+            lowcut=1,
+            highcut=1.5,
+        ),
     ],
-    sampling_rate=sampling_rate,
-    standardize=True,
 )
+
+# nk.video_plot([video, faces], frames=5, signals=[data["pyVHR"], data["ECG_Rate"]])
